fb5e3d97c65697d56c5a0df9017e21e89549c531
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2016-08-09 19:46:10 +0200

    navbar totally changed

---
+++ js/js.js
@@ -0,0 +1,9 @@
+// alert("halo");
+
+$(document).ready(function(){
+    /* affix the navbar after scroll below header */
+    $(".navbar").affix({offset: {top: $("header").outerHeight(true)} 
+    
+    });
+
+});
