afd447930f9fd0e809ac62d8b904ac4bf6037823
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2016-08-13 17:54:35 +0200

    effect on skills section

--- js/js.js
+++ js/js.js
@@ -6,12 +6,23 @@ $(document).ready(function(){
     
     });
 
-  $("#project1, #project2, #project3, #project4").hide();
+  $("#project1, #project2, #project3, #project4, #project5").hide();
 
     $("#projectsChapter").click( function() {
-        $('#project1, #project2, #project3, #project4').slideToggle();
-        $(this).html($(this).html() == 'Hide Projects' ? 'Show Projects' : 'Hide Projects');                    
+        $('#project1, #project2, #project3, #project4, #project5').slideToggle();
+        $(this).html($(this).html() == 'Hide Projects' ? 'Show Projects' : 'Hide Projects'); 
+        // console.log("working");                   
     });
 
+    $("#skills").click(function(){
+     $(this).animate({
+    color: "green",
+    backgroundColor: "rgb( 20, 20, 20 )"
+  });
+     $(this).animate({
+    color: "red",
+    backgroundColor: "white"
+  });
+});
 
 });
