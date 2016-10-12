74493990efc08b1b7fce8165a44bdee29a7ce8ae
“Matteo git config --global user.email “matteo.soresini@hotmail.it <matteo.soresini@hotmail.it>
2016-10-12 14:44:18 +0200

    <head> ok

--- js/js.js
+++ js/js.js
@@ -25,105 +25,105 @@ var loop = setInterval(function(){
     
 }, 30);
 //     /* affix the navbar after scroll below header */
-//     $(".navbar").affix({offset: {top: $("header").outerHeight(true)} 
+    $(".navbar").affix({offset: {top: $("header").outerHeight(true)} 
     
-//     });
+    });
 
-//   $("#project1, #project2, #project3, #project4, #project5").hide();
+  $("#project1, #project2, #project3, #project4, #project5").hide();
 
-//     $("#projectsChapter").click( function() {
-//         $('#project1, #project2, #project3, #project4, #project5').slideToggle();
-//         $(this).html($(this).html() == 'Hide Projects' ? 'Show Projects' : 'Hide Projects'); 
-//         console.log("working");                   
-//     });
+    $("#projectsChapter").click( function() {
+        $('#project1, #project2, #project3, #project4, #project5').slideToggle();
+        $(this).html($(this).html() == 'Hide Projects' ? 'Show Projects' : 'Hide Projects'); 
+        console.log("working");                   
+    });
 
-//     $("#skillsbtn").click(function(){
-//      $("#skills").animate({
-//     color: "#F44336"
-//   });
-//      $("#skills").effect("shake");
-// });
+    $("#skillsbtn").click(function(){
+     $("#skills").animate({
+    color: "#F44336"
+  });
+     $("#skills").effect("shake");
+});
 
-//     $('#ChangeToggle').click(function() {
-//     $('#navbar-hamburger').toggleClass('hidden');
-//     $('#navbar-close').toggleClass('hidden');  
-//   });
+    $('#ChangeToggle').click(function() {
+    $('#navbar-hamburger').toggleClass('hidden');
+    $('#navbar-close').toggleClass('hidden');  
+  });
 
-//     $(document).ready(function(){
-//     $("div[name=animate]").each(function(){
-//         animateDiv($(this));
-//     });
-// });
+    $(document).ready(function(){
+    $("div[name=animate]").each(function(){
+        animateDiv($(this));
+    });
+});
 
-// function makeNewPosition(){
+function makeNewPosition(){
     
-//     // Get viewport dimensions (remove the dimension of the div)
-//     var h = $(window).height() - 50;
-//     var w = $(window).width() - 50;
+    // Get viewport dimensions (remove the dimension of the div)
+    var h = $(window).height() - 50;
+    var w = $(window).width() - 50;
     
-//     var nh = Math.floor(Math.random() * h);
-//     var nw = Math.floor(Math.random() * w);
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
     
-//     return [nh,nw];    
+    return [nh,nw];    
     
-// }
+}
 
-// function animateDiv(c){
-//     var newq = makeNewPosition();
-//     $(c).animate({ top: newq[0], left: newq[1], right: newq[2], bottom: newq[3] }, 3000 / speed, function(){
-//       animateDiv(c);        
-//     });
+function animateDiv(c){
+    var newq = makeNewPosition();
+    $(c).animate({ top: newq[0], left: newq[1], right: newq[2], bottom: newq[3] }, 3000 / speed, function(){
+      animateDiv(c);        
+    });
     
-// };
+};
 
-// $('.circle1').click(function(){
-//    $(this).effect('explode');
-//    console.log("explode HTML5");
-//    setTimeout(function(){ window.location = "http://www.html5.com/"; }, delay);
+$('.circle1').click(function(){
+   $(this).effect('explode');
+   console.log("explode HTML5");
+   setTimeout(function(){ window.location = "http://www.html5.com/"; }, delay);
    
-//    });
+   });
 
-// $('.circle2').click(function(){
-//    $(this).effect('explode');
-//    console.log("explode CSS3");
-//    setTimeout(function(){ window.location = "http://www.css3.com/"; }, delay);
+$('.circle2').click(function(){
+   $(this).effect('explode');
+   console.log("explode CSS3");
+   setTimeout(function(){ window.location = "http://www.css3.com/"; }, delay);
    
-//    });
+   });
 
-// $('.circle3').click(function(){
-//    $(this).effect('explode');
-//    console.log("explode");
-//    setTimeout(function(){ window.location = "http://rubyonrails.org/"; }, delay);
+$('.circle3').click(function(){
+   $(this).effect('explode');
+   console.log("explode");
+   setTimeout(function(){ window.location = "http://rubyonrails.org/"; }, delay);
    
-//    });
+   });
 
-// $('.circle4').click(function(){
-//    $(this).effect('explode');
-//    console.log("explode");
-//    setTimeout(function(){ window.location = "https://nodejs.org/en/"; }, delay);
+$('.circle4').click(function(){
+   $(this).effect('explode');
+   console.log("explode");
+   setTimeout(function(){ window.location = "https://nodejs.org/en/"; }, delay);
    
-//    });
+   });
 
-// $('.circle5').click(function(){
-//    $(this).effect('explode');
-//    console.log("explode");
-//    setTimeout(function(){ window.location = "https://angularjs.org/"; }, delay);
+$('.circle5').click(function(){
+   $(this).effect('explode');
+   console.log("explode");
+   setTimeout(function(){ window.location = "https://angularjs.org/"; }, delay);
    
-//    });
+   });
 
-// $('.circle6').click(function(){
-//    $(this).effect('explode');
-//    console.log("explode");
-//    setTimeout(function(){ window.location = "https://www.javascript.com/"; }, delay);
+$('.circle6').click(function(){
+   $(this).effect('explode');
+   console.log("explode");
+   setTimeout(function(){ window.location = "https://www.javascript.com/"; }, delay);
    
-//    });
+   });
 
-// $('.circle7').click(function(){
-//    $(this).effect('explode');
-//    console.log("explode");
-//    setTimeout(function(){ window.location = "https://jquery.com/"; }, delay);
+$('.circle7').click(function(){
+   $(this).effect('explode');
+   console.log("explode");
+   setTimeout(function(){ window.location = "https://jquery.com/"; }, delay);
    
-//    });
+   });
 
 });
 
